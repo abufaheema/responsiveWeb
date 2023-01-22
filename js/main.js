d262a934d87b8782833123d7644bda43d5abc029
abufaheema <oye1965@gmail.com>
2023-01-22 11:19:22 +0100

    fx issue

--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@ $('document').ready(function () {
         dropRadius: 5,
         perturbance: 0.01,
         
-      });
+    });
+    
+    $('.text').typed({
+        strings: ['first sentence', 'second sentence'],
+        typespeed: 0,
+        loop: true
+    });
 
 
 
